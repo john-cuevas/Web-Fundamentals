f86a2fb961112c10e4a5f0e4a651a0ec218e1e47
John Cuevas <jpmcuevas20Gmail.com>
2022-02-01 20:26:20 -0800

    Likes

---
+++ W2/D2/Likes/script.js
@@ -0,0 +1,23 @@
+var count = 9;
+var countElement = document.querySelector(".like1")
+
+function addlike(element){
+    count++;
+    countElement.innerText = count + "  like(s)";
+}
+
+var count2 = 12;
+var countElement2 = document.querySelector(".like2")
+
+function addlike2(element){
+    count2++;
+    countElement2.innerText = count2 + "  like(s)";
+}
+
+var count3 = 9;
+var countElement3 = document.querySelector(".like3")
+
+function addlike3(element){
+    count3++;
+    countElement3.innerText = count3 + "  like(s)";
+}
